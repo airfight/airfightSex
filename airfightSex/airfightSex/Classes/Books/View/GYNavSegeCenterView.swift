b6298560f1fe889e265df6ef16ad54c44bdc9b1a
airfight <745756619@qq.com>
2016-10-18 22:55:43 +0800

    //顶部nav

---
+++ airfightSex/airfightSex/Classes/Books/View/GYNavSegeCenterView.swift
@@ -0,0 +1,22 @@
+
+//
+//  GYNavSegeCenterView.swift
+//  airfightSex
+//
+//  Created by giant on 16/10/18.
+//  Copyright © 2016年 giant. All rights reserved.
+//
+
+import UIKit
+
+class GYNavSegeCenterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
